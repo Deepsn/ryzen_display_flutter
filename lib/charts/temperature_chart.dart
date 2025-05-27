de3b314e73e7e6575fc91a0d28c09e1eccb042e8
Deepsn <75620590+Deepsn@users.noreply.github.com>
2025-05-27 11:31:43 -0300

    feat(temperature_chart): use cartesian chart with custom axis

--- lib/charts/temperature_chart.dart
+++ lib/charts/temperature_chart.dart
@@ -1,8 +1,16 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:ryzen_display_flutter/adj_widget.dart';
 import 'package:ryzen_display_flutter/hooks/use_timer.dart';
-import 'package:syncfusion_flutter_charts/sparkcharts.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+class TemperatureData {
+  TemperatureData(this.index, this.temperature);
+
+  final int index;
+  final double temperature;
+}
 
 class TemperatureChart extends AdjWidget {
   const TemperatureChart({super.key, required super.adj});
@@ -10,6 +18,7 @@ class TemperatureChart extends AdjWidget {
   @override
   Widget build(BuildContext context) {
     final data = useState<List<double>>([]);
+    final maxTemperature = useState(100.0);
 
     useTimer(Duration(seconds: 1), (timer) {
       data.value = [...data.value, adj.getTCTLTemp()];
@@ -17,15 +26,35 @@ class TemperatureChart extends AdjWidget {
       if (data.value.length > 15) {
         data.value.removeAt(0);
       }
+
+      maxTemperature.value = adj.getTCTLTempLimit() + 5;
     });
 
     return Expanded(
-      child: SfSparkAreaChart(
-        data: data.value,
-        color: Colors.blue.withAlpha((255 / 2).round()),
-        borderColor: Colors.blue,
-        borderWidth: 3,
+      child: SfCartesianChart(
+        plotAreaBorderWidth: 0,
+        primaryXAxis: const NumericAxis(majorGridLines: MajorGridLines(width: 0), isVisible: false),
+        primaryYAxis: CategoryAxis(
+          axisLine: AxisLine(width: 0),
+          majorTickLines: MajorTickLines(size: 0),
+          maximum: maxTemperature.value,
+          minimum: 1,
+        ),
+
+        series: <LineSeries<TemperatureData, num>>[
+          LineSeries<TemperatureData, num>(
+            dataSource: _getChartData(data.value),
+            xValueMapper: (TemperatureData data, _) => data.index,
+            yValueMapper: (TemperatureData data, _) => data.temperature,
+            animationDuration: 0,
+            animationDelay: 0,
+          ),
+        ],
       ),
     );
+  }
+
+  List<TemperatureData> _getChartData(List<double> data) {
+    return data.mapIndexed((index, temperature) => TemperatureData(index, temperature)).toList();
   }
 }
